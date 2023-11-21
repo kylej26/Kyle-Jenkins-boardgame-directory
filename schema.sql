0cde5bb88ca5068d66036b50faad86f1a04ba528
Kyle Jenkins <90466681+kylej26@users.noreply.github.com>
2023-11-21 10:55:32 -0500

    Update schema.sql
    missing DELIMETER wrapper on triggers causing the error

--- schema.sql
+++ schema.sql
@@ -1,50 +1,52 @@
-# TODO: add mysql/mariadb table definitions
+-- TODO: add mysql/mariadb table definitions
 CREATE TABLE `game` (
-	`gameid` INT(30) NOT NULL AUTO_INCREMENT,
+	`gameid` INT(11) NOT NULL AUTO_INCREMENT,
 	`Name` VARCHAR(30) NOT NULL,
-	`times_played` INT(30) NULL DEFAULT 0,
+	`times_played` INT(11) NULL DEFAULT 0,
 	`MostRecent` TIMESTAMP NULL DEFAULT NULL,
 	PRIMARY KEY (`gameid`) 
 );
 
 CREATE TABLE `session` (
-	`session_id` INT(30) NOT NULL AUTO_INCREMENT,
-	`gameid` INT(30) NOT NULL,
+	`session_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`gameid` INT(11) NOT NULL,
 	`Date` TIMESTAMP NULL DEFAULT NULL,
 	PRIMARY KEY (`session_id`),
 	INDEX `fk_gameid` (`gameid`),
 	CONSTRAINT `fk_gameid` FOREIGN KEY (`gameid`) REFERENCES `game` (`gameid`) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 
-CREATE TRIGGER update_most_recent_session
-AFTER INSERT ON session
+DELIMITER $$
+
+CREATE TRIGGER `update_most_recent_session`
+AFTER INSERT ON `session`
 FOR EACH ROW
 BEGIN
-    UPDATE Game
-    SET MostRecentSessionDate = (
-        SELECT MAX(dateColumn)
-        FROM GameSession
-        WHERE GameID = NEW.GameID
+    UPDATE `game`
+    SET `MostRecent` = (
+        SELECT MAX(`Date`)
+        FROM `session`
+        WHERE `gameid` = NEW.`gameid`
     )
-    WHERE GameID = NEW.GameID;
+    WHERE `gameid` = NEW.`gameid`;
 END;
- 
+$$
 
- CREATE TRIGGER update_most_times_played
- AFTER INSERT ON session
- FOR EACH ROW
- BEGIN
-	UPDATE game
-   SET times_played = times_played + 1
-   WHERE gameid = NEW.gameid;
+CREATE TRIGGER `update_most_times_played`
+AFTER INSERT ON `session`
+FOR EACH ROW
+BEGIN
+	UPDATE `game`
+    SET `times_played` = `times_played` + 1
+    WHERE `gameid` = NEW.`gameid`;
 END;
+$$
 
-# TODO: add sample data
-INSERT INTO Game (Name) VALUES ('LIFE');
+DELIMITER ;
 
- INSERT INTO Game (Name) VALUES ('Monopoly');
+-- TODO: add sample data
+INSERT INTO `game` (`Name`) VALUES ('LIFE');
 
- INSERT INTO Game (Name) VALUES ('Scrabble');
+INSERT INTO `game` (`Name`) VALUES ('Monopoly');
 
-
-
+INSERT INTO `game` (`Name`) VALUES ('Scrabble');
